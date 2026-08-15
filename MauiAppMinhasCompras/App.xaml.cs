6a1179d7743bd099a65ff98b197c9df7e69cf873
glaucia <glauciamariano19-alt>
2026-08-15 18:42:29 -0300

    Add models e troca da tela inicial

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -11,7 +11,10 @@ namespace MauiAppMinhasCompras
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new AppShell());
+            //return new Window(new AppShell());
+            return new Window(new NavigationPage(new MauiAppMinhasCompras.Views.ListaProduto()));
+
+
         }
     }
 }
--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -1,6 +1,6 @@
 namespace MauiAppMinhasCompras.Views;
 
-public partial class ListaProduto : ContentView
+public partial class ListaProduto : ContentPage
 {
 	public ListaProduto()
 	{
