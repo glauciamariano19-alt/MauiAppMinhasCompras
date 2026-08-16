3ba7caae404164863e32485c7a1756b265393d84
glaucia <glauciamariano19-alt>
2026-08-15 22:55:41 -0300

    Construção da classe para fazer o CRUD no SQLite e correção de variáveis

--- MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
+++ MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
@@ -1,10 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using MauiAppMinhasCompras.Models;
+using SQLite;
 
 namespace MauiAppMinhasCompras.Helpers
 {
-    internal class SQLiteDatabaseHelper
+    public class SQLiteDatabaseHelper
     {
+        readonly SQLiteAsyncConnection connection;
+
+        public SQLiteDatabaseHelper(string path) 
+        {
+            connection = new SQLiteAsyncConnection(path);
+            connection.CreateTableAsync<Produto>().Wait();
+        }
+
+        public Task<int> Insert(Produto p) 
+        { 
+            return connection.InsertAsync(p);
+        }
+
+        public Task<List<Produto>> Update(Produto p)
+        {
+            string sql = "UPDATE Produto SET Descricao=?, Quantidade=?, Preco=? Where ID=?";
+
+            return connection.QueryAsync<Produto>(
+                sql, p.Descricao, p.Quantidade,p.Preco, p.Id
+          );
+        }
+
+        public Task<int> Delete(int id)
+        {
+            return connection.Table<Produto>().DeleteAsync(i => i.Id == id);
+        }
+
+
+        public Task<List<Produto>> GetAll() 
+        {
+            return connection.Table<Produto>().ToListAsync();
+        }
+        public Task<List<Produto>> Search(string q) 
+        {
+            string sql = "SELECT * Produto WHERE descricao LIKE '%" + q + "%'";
+
+            return connection.QueryAsync<Produto>(sql);
+        } 
     }
 }
--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -2,10 +2,10 @@
 
 namespace MauiAppMinhasCompras.Models
 {
-    internal class Produto
+    public class Produto
     {
         [PrimaryKey, AutoIncrement]
-        public string Id { get; set; }
+        public int Id { get; set; }
         public string Descricao { get; set; }
         public double Quantidade { get; set; }
         public double Preco { get; set; }
